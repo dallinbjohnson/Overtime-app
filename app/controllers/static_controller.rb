408b6cf968dcd44f117e5427b4d6a323dde22e7d
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-07-21 15:44:26 -0600

    Implemented functional buttons for employee homepage confirmations

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,7 +4,7 @@ class StaticController < ApplicationController
 			@pending_approvals = Post.submitted
 			@recent_audit_items = AuditLog.last(10)
 		else
-			@pending_audit_confimations = current_user.audit_logs
+			@pending_audit_confimations = current_user.audit_logs.pending
 		end
 	end
-end
+endhttp://localhost:3000/audit_logs/2/confirm
